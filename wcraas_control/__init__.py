0fadd9e20c127a384661cd212a45930033b09f96
Kolokotronis Panagiotis <panos122008@gmail.com>
2019-10-28 21:18:21 +0100

    Bump version: 0.1.1 → 0.1.2

--- wcraas_control/__init__.py
+++ wcraas_control/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Kolokotronis Panagiotis"""
 __email__ = "panagiks@gmail.com"
-__version__ = "__version__ = '0.1.1'"
+__version__ = "__version__ = '0.1.2'"
 
 from wcraas_control.config import Config  # NOQA
 from wcraas_control.wcraas_control import ControlWorker  # NOQA
